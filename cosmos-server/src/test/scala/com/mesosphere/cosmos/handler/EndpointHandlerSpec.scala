00f033b321c94c248bd4461e4c3bfbd71d24c0e7
Charles Ruhland <charles@mesosphere.io>
2016-05-26 16:07:48 -0700

    Refactor EndpointHandlerSpec

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/handler/EndpointHandlerSpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/handler/EndpointHandlerSpec.scala
@@ -1,12 +1,13 @@
 package com.mesosphere.cosmos.handler
 
+import cats.Eval
 import cats.data.Xor
 import com.mesosphere.cosmos.UnitSpec
 import com.mesosphere.cosmos.http.{MediaTypes, RequestSession}
 import com.twitter.finagle.http.{Method, Request, RequestBuilder}
 import com.twitter.util.{Await, Future}
 import io.circe.{Encoder, Json}
-import io.finch.{Endpoint, Input, RequestReader}
+import io.finch.{Endpoint, Input, Output, RequestReader}
 
 final class EndpointHandlerSpec extends UnitSpec {
 
@@ -14,36 +15,35 @@ final class EndpointHandlerSpec extends UnitSpec {
 
     "route()" - {
 
+      type EndpointResult = Option[(Input, Eval[Future[Output[Json]]])]
+
       "HTTP method parameter" - {
 
         "route get matches request get" in {
-          assertMethodMatch(routeMethod = Method.Get, requestMethod = Method.Get)
+          val result = callWithMethods(routeMethod = Method.Get, requestMethod = Method.Get)
+          assertSuccessfulRequest(result)
         }
 
         "route get does not match request post" in {
-          assertMethodMismatch(endpointMethod = Method.Get, requestMethod = Method.Post)
+          val result = callWithMethods(routeMethod = Method.Get, requestMethod = Method.Post)
+          assertFailedRequest(result)
         }
 
         "route post does not match request get" in {
-          assertMethodMismatch(endpointMethod = Method.Post, requestMethod = Method.Get)
+          val result = callWithMethods(routeMethod = Method.Post, requestMethod = Method.Get)
+          assertFailedRequest(result)
         }
 
         "route post matches request post" in {
-          assertMethodMatch(routeMethod = Method.Post, requestMethod = Method.Post)
+          val result = callWithMethods(routeMethod = Method.Post, requestMethod = Method.Post)
+          assertSuccessfulRequest(result)
         }
 
-        def assertMethodMatch(routeMethod: Method, requestMethod: Method): Unit = {
+        def callWithMethods(routeMethod: Method, requestMethod: Method): EndpointResult = {
           val endpoint = buildEndpoint(method = routeMethod)
           val request = buildRequest(method = requestMethod)
 
-          assertSuccessfulRequest(endpoint, request)
-        }
-
-        def assertMethodMismatch(endpointMethod: Method, requestMethod: Method): Unit = {
-          val endpoint = buildEndpoint(method = endpointMethod)
-          val request = buildRequest(method = requestMethod)
-
-          assertFailedRequest(endpoint, request)
+          callEndpoint(endpoint, request)
         }
 
       }
@@ -51,34 +51,32 @@ final class EndpointHandlerSpec extends UnitSpec {
       "path parameter" - {
 
         "route path foo/bar matches request path foo/bar" in {
-          assertPathMatch(routePath = Seq("foo", "bar"), requestPath = Seq("foo", "bar"))
+          val result = callWithPaths(routePath = Seq("foo", "bar"), requestPath = Seq("foo", "bar"))
+          assertSuccessfulRequest(result)
         }
 
         "route path foo/bar does not match request path foo/baz" in {
-          assertPathMismatch(routePath = Seq("foo", "bar"), requestPath = Seq("foo", "baz"))
+          val result = callWithPaths(routePath = Seq("foo", "bar"), requestPath = Seq("foo", "baz"))
+          assertFailedRequest(result)
         }
 
         "route path foo/baz does not match request path foo/bar" in {
-          assertPathMismatch(routePath = Seq("foo", "baz"), requestPath = Seq("foo", "bar"))
+          val result = callWithPaths(routePath = Seq("foo", "baz"), requestPath = Seq("foo", "bar"))
+          assertFailedRequest(result)
         }
 
         "route path foo/baz matches request path foo/baz" in {
-          assertPathMatch(routePath = Seq("foo", "baz"), requestPath = Seq("foo", "baz"))
+          val result = callWithPaths(routePath = Seq("foo", "baz"), requestPath = Seq("foo", "baz"))
+          assertSuccessfulRequest(result)
         }
 
-        def assertPathMatch(routePath: Seq[String], requestPath: Seq[String]): Unit = {
+        def callWithPaths(routePath: Seq[String], requestPath: Seq[String]): EndpointResult = {
           val endpoint = buildEndpoint(path = routePath)
           val request = buildRequest(path = requestPath)
 
-          assertSuccessfulRequest(endpoint, request)
+          callEndpoint(endpoint, request)
         }
 
-        def assertPathMismatch(routePath: Seq[String], requestPath: Seq[String]): Unit = {
-          val endpoint = buildEndpoint(path = routePath)
-          val request = buildRequest(path = requestPath)
-
-          assertFailedRequest(endpoint, request)
-        }
       }
 
       def buildEndpoint(
@@ -104,17 +102,19 @@ final class EndpointHandlerSpec extends UnitSpec {
           .build(method, content = None)
       }
 
-      def assertSuccessfulRequest(endpoint: Endpoint[Json], request: Request): Unit = {
-        inside(endpoint(Input(request))) { case Some((_, eval)) =>
-          inside(Await.result(eval.value).value.as[Unit]) { case Xor.Right(result) =>
-            assertResult(())(result)
+      def callEndpoint(endpoint: Endpoint[Json], request: Request): EndpointResult = {
+        endpoint(Input(request))
+      }
+
+      def assertSuccessfulRequest(result: EndpointResult): Unit = {
+        inside(result) { case Some((_, eval)) =>
+          inside(Await.result(eval.value).value.as[Unit]) { case Xor.Right(responseBody) =>
+            assertResult(())(responseBody)
           }
         }
       }
 
-      def assertFailedRequest(endpoint: Endpoint[Json], request: Request): Unit = {
-        assertResult(None)(endpoint(Input(request)))
-      }
+      def assertFailedRequest(result: EndpointResult): Unit = assertResult(None)(result)
 
     }
 
