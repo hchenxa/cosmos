5ae9bf37942a99fb2d664f2c2f911759e0e11bc1
Charles Ruhland <charles@mesosphere.io>
2016-05-26 15:52:18 -0700

    Tests for `path` parameter of EndpointHandler.route

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/handler/EndpointHandlerSpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/handler/EndpointHandlerSpec.scala
@@ -14,7 +14,7 @@ final class EndpointHandlerSpec extends UnitSpec {
 
     "route()" - {
 
-      "method parameter" - {
+      "HTTP method parameter" - {
 
         "route get matches request get" in {
           assertMethodMatch(routeMethod = Method.Get, requestMethod = Method.Get)
@@ -32,42 +32,88 @@ final class EndpointHandlerSpec extends UnitSpec {
           assertMethodMatch(routeMethod = Method.Post, requestMethod = Method.Post)
         }
 
-        def buildEndpoint(method: Method): Endpoint[Json] = {
-          val context = EndpointContext((), RequestSession(None), identity[Unit], MediaTypes.any)
-          val reader = RequestReader.value(context)
-          implicit val codec = EndpointCodec(reader, implicitly[Encoder[Unit]])
-
-          val handler = new EndpointHandler {
-            override def apply(request: Unit)(implicit session: RequestSession): Future[Unit] = {
-              Future.value(())
-            }
-          }
-
-          handler.testRoute(method)
-        }
-
-        def buildRequest(method: Method): Request = {
-          RequestBuilder().url("http://some.host/").build(method, content = None)
-        }
-
         def assertMethodMatch(routeMethod: Method, requestMethod: Method): Unit = {
-          val endpoint = buildEndpoint(routeMethod)
-          val request = buildRequest(requestMethod)
+          val endpoint = buildEndpoint(method = routeMethod)
+          val request = buildRequest(method = requestMethod)
 
-          inside(endpoint(Input(request))) { case Some((_, eval)) =>
-            inside(Await.result(eval.value).value.as[Unit]) { case Xor.Right(result) =>
-              assertResult(())(result)
-            }
-          }
+          assertSuccessfulRequest(endpoint, request)
         }
 
         def assertMethodMismatch(endpointMethod: Method, requestMethod: Method): Unit = {
-          val endpoint = buildEndpoint(endpointMethod)
-          val request = buildRequest(requestMethod)
+          val endpoint = buildEndpoint(method = endpointMethod)
+          val request = buildRequest(method = requestMethod)
 
-          assertResult(None)(endpoint(Input(request)))
+          assertFailedRequest(endpoint, request)
         }
 
+      }
+
+      "path parameter" - {
+
+        "route path foo/bar matches request path foo/bar" in {
+          assertPathMatch(routePath = Seq("foo", "bar"), requestPath = Seq("foo", "bar"))
+        }
+
+        "route path foo/bar does not match request path foo/baz" in {
+          assertPathMismatch(routePath = Seq("foo", "bar"), requestPath = Seq("foo", "baz"))
+        }
+
+        "route path foo/baz does not match request path foo/bar" in {
+          assertPathMismatch(routePath = Seq("foo", "baz"), requestPath = Seq("foo", "bar"))
+        }
+
+        "route path foo/baz matches request path foo/baz" in {
+          assertPathMatch(routePath = Seq("foo", "baz"), requestPath = Seq("foo", "baz"))
+        }
+
+        def assertPathMatch(routePath: Seq[String], requestPath: Seq[String]): Unit = {
+          val endpoint = buildEndpoint(path = routePath)
+          val request = buildRequest(path = requestPath)
+
+          assertSuccessfulRequest(endpoint, request)
+        }
+
+        def assertPathMismatch(routePath: Seq[String], requestPath: Seq[String]): Unit = {
+          val endpoint = buildEndpoint(path = routePath)
+          val request = buildRequest(path = requestPath)
+
+          assertFailedRequest(endpoint, request)
+        }
+      }
+
+      def buildEndpoint(
+        method: Method = Method.Post,
+        path: Seq[String] = Seq.empty
+      ): Endpoint[Json] = {
+        val context = EndpointContext((), RequestSession(None), identity[Unit], MediaTypes.any)
+        val reader = RequestReader.value(context)
+        implicit val codec = EndpointCodec(reader, implicitly[Encoder[Unit]])
+
+        val handler = new EndpointHandler {
+          override def apply(request: Unit)(implicit session: RequestSession): Future[Unit] = {
+            Future.value(())
+          }
+        }
+
+        handler.testRoute(method, path: _*)
+      }
+
+      def buildRequest(method: Method = Method.Post, path: Seq[String] = Seq.empty): Request = {
+        RequestBuilder()
+          .url(s"http://some.host/${path.mkString("/")}")
+          .build(method, content = None)
+      }
+
+      def assertSuccessfulRequest(endpoint: Endpoint[Json], request: Request): Unit = {
+        inside(endpoint(Input(request))) { case Some((_, eval)) =>
+          inside(Await.result(eval.value).value.as[Unit]) { case Xor.Right(result) =>
+            assertResult(())(result)
+          }
+        }
+      }
+
+      def assertFailedRequest(endpoint: Endpoint[Json], request: Request): Unit = {
+        assertResult(None)(endpoint(Input(request)))
       }
 
     }
