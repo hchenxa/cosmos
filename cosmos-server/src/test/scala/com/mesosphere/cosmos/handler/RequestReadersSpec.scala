3ed5127c6fa09aa2989a07b92c4d552bc3577a55
Charles Ruhland <charles@mesosphere.io>
2016-05-31 12:16:42 -0700

    Clean up RequestReadersSpec and add details to test descriptions

--- cosmos-server/src/test/scala/com/mesosphere/cosmos/handler/RequestReadersSpec.scala
+++ cosmos-server/src/test/scala/com/mesosphere/cosmos/handler/RequestReadersSpec.scala
@@ -5,55 +5,87 @@ import com.mesosphere.cosmos.http.{Authorization, MediaType, MediaTypes, Request
 import com.twitter.finagle.http.RequestBuilder
 import com.twitter.io.Buf
 import com.twitter.util.{Await, Return, Try}
-import io.circe.Encoder
-import io.circe.syntax._
+import io.circe.Json
 import io.finch.RequestReader
-import org.scalatest.PrivateMethodTester
 
 final class RequestReadersSpec extends UnitSpec {
 
   import RequestReadersSpec._
 
-  "noBody" - {
+  "The RequestReader built by RequestReaders.noBody should" - {
 
-    behave like baseReader() {
-      new RequestReaderBuilder[Unit] {
-        override def apply[Res](
-          produces: Seq[(MediaType, Res => Res)]
-        ): RequestReader[EndpointContext[Unit, Res]] = RequestReaders.noBody(produces)
+    behave like baseReader(factory = NoBodyReaderFactory)
+
+  }
+
+  "The RequestReader built by RequestReaders.standard should" - {
+
+    behave like baseReader(factory = StandardReaderFactory)
+
+  }
+
+  def baseReader[Req](factory: RequestReaderFactory[Req]): Unit = {
+
+    "include the Authorization header in the return value if it was included in the request" - {
+      "to accurately forward the header's state to other services" in {
+        val Return((requestSession, _, _)) = runReader(authorization = Some("53cr37"))
+        assertResult(RequestSession(Some(Authorization("53cr37"))))(requestSession)
       }
     }
 
-  }
-
-  "standard" - {
-
-    behave like baseReader(contentType = Some(MediaTypes.applicationJson), body = Some("body")) {
-      new RequestReaderBuilder[String] {
-        override def apply[Res](
-          produces: Seq[(MediaType, Res => Res)]
-        ): RequestReader[EndpointContext[String, Res]] = RequestReaders.standard(
-          accepts = MediaTypes.applicationJson,
-          produces = produces
-        )
+    "omit the Authorization header from the return value if it was omitted from the request" - {
+      "to accurately forward the header's state to other services" in {
+        val Return((requestSession, _, _)) = runReader(authorization = None)
+        assertResult(RequestSession(None))(requestSession)
       }
     }
 
-  }
+    "fail if the Accept header is not a value we support" - {
+      "(because we can only encode the response to one of the supported formats)" - {
+        "the Accept header is missing" in {
+          val result = runReader(accept = None)
+          assert(result.isThrow)
+        }
 
-  trait RequestReaderBuilder[Req] {
-    def apply[Res](
-      produces: Seq[(MediaType, Res => Res)]
-    ): RequestReader[EndpointContext[Req, Res]]
-  }
+        "the Accept header cannot be decoded as a MediaType" in {
+          val result = runReader(accept = Some("---not-a-media-type---"))
+          assert(result.isThrow)
+        }
 
-  def baseReader[Req: Encoder](contentType: Option[MediaType] = None, body: Option[Req] = None)(
-    builder: RequestReaderBuilder[Req]
-  ): Unit = {
+        "the Accept header is not compatible with a MediaType in `produces`" - {
+          "where `produces` is empty" in {
+            val result = runReader(produces = Seq.empty)
+            assert(result.isThrow)
+          }
+          "where `produces` contains only incompatible header values" in {
+            val result = runReader(accept = Some("text/plain"))
+            assert(result.isThrow)
+          }
+        }
+      }
+    }
 
-    val encodedBody: Buf = body match {
-      case Some(b) => Buf.Utf8(b.asJson.noSpaces)
-      case _ => Buf.Empty
+    "if the Accept header has a value we support" - {
+      "include the corresponding Content-Type in the return value" - {
+        "so that it can be included as a header in the response" in {
+          val mediaType = MediaType("application", "json")
+          val produces = Seq((mediaType, identity[Unit] _))
+          val Return((_, _, responseContentType)) = runReader(produces = produces)
+          assertResult(mediaType)(responseContentType)
+        }
+      }
+
+      "include the first corresponding response formatting function in the return value" - {
+        "so that it can be used to correctly format the response" in {
+          val produces = Seq[(MediaType, Int => Int)](
+            (MediaType("text", "plain"), _ => 0),
+            (MediaTypes.applicationJson, _ => 1),
+            (MediaTypes.applicationJson, _ => 2)
+          )
+          val Return((_, responseFormatter, _)) = runReader(produces = produces)
+          assertResult(1)(responseFormatter(42))
+        }
+      }
     }
 
     def runReader[Res](
@@ -65,69 +97,51 @@ final class RequestReadersSpec extends UnitSpec {
         .url("http://some.host")
         .setHeader("Accept", accept.toSeq)
         .setHeader("Authorization", authorization.toSeq)
-        .setHeader("Content-Type", contentType.map(_.show).toSeq)
-        .buildPost(encodedBody)
+        .setHeader("Content-Type", MediaTypes.applicationJson.show)
+        .buildPost(Buf.Utf8(Json.empty.noSpaces))
 
-      val reader = builder(produces)
+      val reader = factory(produces)
       Await.result(reader(request).liftToTry).map { context =>
         (context.session, context.responseFormatter, context.responseContentType)
       }
-    }
-
-    "Result contains Authorization if the request did" in {
-      val Return((requestSession, _, _)) = runReader(authorization = Some("53cr37"))
-      assertResult(RequestSession(Some(Authorization("53cr37"))))(requestSession)
-    }
-
-    "Result omits Authorization if the request did" in {
-      val Return((requestSession, _, _)) = runReader(authorization = None)
-      assertResult(RequestSession(None))(requestSession)
-    }
-
-    "Fails if the Accept header is missing" in {
-      val result = runReader(accept = None)
-      assert(result.isThrow)
-    }
-
-    "Fails if the Accept header is not a MediaType" in {
-      val result = runReader(accept = Some("---not-a-media-type---"))
-      assert(result.isThrow)
-    }
-
-    "Fails if the Accept header is not compatible with a MediaType in `produces`" - {
-      "empty `produces`" in {
-        val result = runReader(produces = Seq.empty)
-        assert(result.isThrow)
-      }
-      "incompatible header value" in {
-        val result = runReader(accept = Some("text/plain"))
-        assert(result.isThrow)
-      }
-    }
-
-    "Result contains the compatible Accept header value" in {
-      val mediaType = MediaType("application", "json")
-      val produces = Seq((mediaType, identity[Unit] _))
-      val Return((_, _, responseContentType)) = runReader(produces = produces)
-      assertResult(mediaType)(responseContentType)
-    }
-
-    "Result contains the function associated with the first compatible `produces` element" in {
-      val produces = Seq[(MediaType, Int => Int)](
-        (MediaType("text", "plain"), _ => 0),
-        (MediaTypes.applicationJson, _ => 1),
-        (MediaTypes.applicationJson, _ => 2)
-      )
-      val Return((_, responseFormatter, _)) = runReader(produces = produces)
-      assertResult(1)(responseFormatter(42))
     }
 
   }
 
 }
 
-object RequestReadersSpec extends PrivateMethodTester {
+object RequestReadersSpec {
 
   type BaseReadValues[Res] = (RequestSession, Res => Res, MediaType)
+
+  /** This factory trait is needed because the `Req` type is different for each factory function in
+    * [[RequestReaders]], but the `Res` type can be different for each test case in `baseReader`.
+    *
+    * Thus, we cannot just use a factory method, because that would couple `Req` and `Res` together.
+    */
+  trait RequestReaderFactory[Req] {
+    def apply[Res](
+      produces: Seq[(MediaType, Res => Res)]
+    ): RequestReader[EndpointContext[Req, Res]]
+  }
+
+  object NoBodyReaderFactory extends RequestReaderFactory[Unit] {
+    override def apply[Res](
+      produces: Seq[(MediaType, Res => Res)]
+    ): RequestReader[EndpointContext[Unit, Res]] = {
+      RequestReaders.noBody(produces)
+    }
+  }
+
+  object StandardReaderFactory extends RequestReaderFactory[String] {
+    override def apply[Res](
+      produces: Seq[(MediaType, Res => Res)]
+    ): RequestReader[EndpointContext[String, Res]] = {
+      RequestReaders.standard(
+        accepts = MediaTypes.applicationJson,
+        produces = produces
+      )
+    }
+  }
 
 }
